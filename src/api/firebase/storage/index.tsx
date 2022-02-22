34456c3570b17fdbadb62db25b84fd3f5e656faa
小林大斗 <khiroto@kobayashidaitonoMacBook-Pro.local>
2022-02-22 21:45:45 +0900

    募集投稿の画像処理を作成

--- src/api/firebase/storage/index.tsx
+++ src/api/firebase/storage/index.tsx
@@ -2,16 +2,39 @@ import { getDownloadURL, listAll, ref, uploadBytes } from "firebase/storage";
 import { storage } from "../setting";
 
 export const uploadUserIcon = async (file: File, uid: string) => {
-  const storageRef = ref(storage, `images/${uid}/userIcon/src`);
+  const storageRef = ref(storage, `images/userIcon/${uid}/src`);
   await uploadBytes(storageRef, file);
 };
 
 export const downloadUserIconURL = async (uid: string): Promise<string> => {
-  const storageRef = ref(storage, `images/${uid}/userIcon/src`);
+  const storageRef = ref(storage, `images/userIcon/${uid}/src`);
 
   // 存在するかどうか調べる
-  const { items } = await listAll(ref(storage, `images/${uid}/userIcon`));
+  const { items } = await listAll(ref(storage, `images/userIcon/${uid}`));
 
+  if (items.length === 0) return "";
+  return await getDownloadURL(storageRef);
+};
+
+export const uploadRequirementImage = async (
+  file: File,
+  uid: string,
+  requirementId: string
+) => {
+  const storageRef = ref(storage, `images/requirement/${requirementId}/src`);
+  await uploadBytes(storageRef, file);
+};
+
+export const downloadRequirementURL = async (
+  requirementId: string
+): Promise<string> => {
+  const storageRef = ref(storage, `images/requirement/${requirementId}/src`);
+
+  const { items } = await listAll(
+    ref(storage, `images/requirement/${requirementId}`)
+  );
+
+  // 存在するかどうか調べる
   if (items.length === 0) return "";
   return await getDownloadURL(storageRef);
 };
